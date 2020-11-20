481da54598afce0bf72d7326954969c7363e6a6a
Afriza Aulia N <kakaafriz@gmail.com>
2020-11-20 11:52:34 +0700

    Membaca text file dan membaca command Insert, update delete, dan traverse untuk setiap tree.

--- ProyekSDL/Form1.Designer.cs
+++ ProyekSDL/Form1.Designer.cs
@@ -28,20 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTestCase = new System.Windows.Forms.Label();
+            this.cbMode = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "select test case";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "selected test case:";
+            // 
+            // lblTestCase
+            // 
+            this.lblTestCase.AutoSize = true;
+            this.lblTestCase.Location = new System.Drawing.Point(114, 67);
+            this.lblTestCase.Name = "lblTestCase";
+            this.lblTestCase.Size = new System.Drawing.Size(59, 13);
+            this.lblTestCase.TabIndex = 2;
+            this.lblTestCase.Text = "<testcase>";
+            // 
+            // cbMode
+            // 
+            this.cbMode.FormattingEnabled = true;
+            this.cbMode.Items.AddRange(new object[] {
+            "rbt",
+            "avl"});
+            this.cbMode.Location = new System.Drawing.Point(117, 94);
+            this.cbMode.Name = "cbMode";
+            this.cbMode.Size = new System.Drawing.Size(121, 21);
+            this.cbMode.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "tree: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 601);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbMode);
+            this.Controls.Add(this.lblTestCase);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTestCase;
+        private System.Windows.Forms.ComboBox cbMode;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ProyekSDL/Form1.cs
+++ ProyekSDL/Form1.cs
@@ -12,9 +12,80 @@ namespace ProyekSDL
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileOpener = new OpenFileDialog();
+            fileOpener.DefaultExt = "txt";
+            fileOpener.Title = "buka file test case";
+            fileOpener.Filter = "text files (*.txt)|*.txt|All Files (*.*)|*.*";
+            fileOpener.FilterIndex = 1;
+            fileOpener.CheckFileExists = true;
+            fileOpener.CheckPathExists = true;
+            if (fileOpener.ShowDialog() == DialogResult.OK)
+            {
+                lblTestCase.Text = fileOpener.FileName;
+                string[] testCaseLines = System.IO.File.ReadAllLines(@fileOpener.FileName);
+                string[] commands = new string[3];
+                for (int i = 0; i < testCaseLines.Length; i++)
+                {
+                    commands = testCaseLines[i].Split(' ');
+
+                    //operationModeSelector digunakan untuk memilih mode operasi
+                    //yaitu mode avl tree atau red black tree
+                    //keyword untuk avl adalah "avl"
+                    //sedangkan red black tree adalah "rbt". penggunaan= operationModeSelector(mode, command, key, value);
+                    string mode = cbMode.Text;//mode diambil dari comboBox cbMode
+
+                    if (commands[0] == "D")//delete
+                        operationModeSelector(mode, commands[0], commands[1]);
+                    else if (commands[0] == "T")//traverse
+                        operationModeSelector(mode, commands[0]);
+                    else
+                        operationModeSelector(mode, commands[0], commands[1], commands[2]);
+                }
+            }
+        }
+
+        void operationModeSelector(string mode, string command, string key = "", string value = "")
+        {
+            if (command == "I")
+            {
+                //Insert
+                if (mode == "rbt")
+                    MessageBox.Show("rbt Insert to node with key: " + key + " the value: " + value);
+                else
+                    MessageBox.Show("avl Insert to node with key: " + key + " the value: " + value);
+            }
+            else if (command == "U")
+            {
+                //Update
+                if(mode=="rbt")
+                    MessageBox.Show("rbt Update node with key: " + key + " with the value: " + value);
+                else
+                    MessageBox.Show("avl Update node with key: " + key + " with the value: " + value);
+            }
+            else if (command == "D")
+            {
+                //Delete
+                if(mode=="rbt")
+                    MessageBox.Show("rbt Delete node with key: " + key);
+                else
+                    MessageBox.Show("avl Delete node with key: " + key);
+            }
+            else if (command == "T")
+            {
+                //Traverse
+                if(mode=="rbt")
+                    MessageBox.Show("RBT WARP MODE ON! STARTING TRAVERSAL! ");
+                else
+                    MessageBox.Show("AVL WARP MODE ON! STARTING TRAVERSAL! ");
+            }
         }
     }
 }
