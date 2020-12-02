4425b48704e7e3c494ae7721e767343776adb910
rnatalia <rnatalia.disineyh@gmail.com>
2020-12-02 20:03:40 +0700

    delete find avl

---
+++ ProyekSDL/ProyekSDL/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyekSDL
+{
+    class Node
+    {
+        public int key, height;
+        public string value;
+        public Node left, right;
+
+        public Node(int d, string val="")
+        {
+            key = d;
+            height = 1;
+            value = val;
+        }
+    }
+}
